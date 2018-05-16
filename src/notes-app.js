7673375873d57b2fe875685ce3e22a1b3f253e4f
Ben Livermore <blivermore@wespire.com>
2018-05-16 09:30:05 -0400

    Add logging, delete, put, ajax.

--- src/notes-app.js
+++ src/notes-app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
@@ -9,18 +10,42 @@ const notes = [
   'http requests have a url, method, header, and body'
 ];
 
+
 app.set('view engine', 'ejs');
+
+app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/css', express.static('src/css'));
+app.use('/js', express.static('src/js'));
 
 app.get('/', (req, res) => {
   res.render('notes', { notes: notes });
 });
 
+app.delete('/notes/:noteId', (req, res) => {
+  const noteId = Number(req.params.noteId);
+  if (noteId >= 0 && noteId < notes.length) {
+    notes.splice(noteId, 1);
+    res.send('Note deleted!');
+  } else {
+    res.status(404).send('Note does not exist!');
+  }
+});
+
+app.put('/notes/:noteId', (req, res) => {
+  const noteId = Number(req.params.noteId);
+  if (noteId >= 0 && noteId < notes.length) {
+    notes[noteId] = req.body.note;
+    res.send('Note updated!');
+  } else {
+    res.status(404).send('Note does not exist!');
+  }
+});
+
 app.post('/notes', (req, res) => {
   notes.push(req.body.note);
-  res.redirect('back');
+  res.send('Note added!');
 });
 
 app.listen(port, () => {
