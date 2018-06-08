6d965429309c89c5618466438c0b45d66eb562f8
Ben Livermore <blivermore@wespire.com>
2018-06-08 16:17:00 -0400

    fix port

--- src/notes-app.js
+++ src/notes-app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const notes = [
   'http is a protocol',
